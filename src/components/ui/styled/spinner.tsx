64640b8f097dc13bf1d81cc24ce2db611a79ce41
Mario Beltrán <belco90@gmail.com>
2024-10-08 16:53:07 +0200

    Fix linting and formatting

--- src/components/ui/styled/spinner.tsx
+++ src/components/ui/styled/spinner.tsx
@@ -1,4 +1,5 @@
 import { ark } from '@ark-ui/react/factory'
+
 import { styled } from 'styled-system/jsx'
 import { spinner } from 'styled-system/recipes'
 import type { ComponentProps } from 'styled-system/types'
