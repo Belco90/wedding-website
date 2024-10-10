f9156626fbe3b549a908cd11364700c8c44d1dc8
Mario Beltrán <belco90@gmail.com>
2024-10-10 23:55:57 +0200

    Add placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 import { Heading } from '@/components/ui/heading'
+import { Text } from '@/components/ui/text'
 
 import { css } from 'styled-system/css'
+import { Center } from 'styled-system/jsx'
 
 export default function HomePage() {
 	return (
-		<main className={css({ background: 'accent.default', height: 'full' })}>
-			<Heading as="h1" textAlign="center" fontFamily="handwriting">
-				¡Nos casamos!
-			</Heading>
+		<main className={css({ background: 'bg.emphasized', height: 'full' })}>
+			<Center
+				flexDirection="column"
+				justifyContent="center"
+				height="full"
+				color="accent.fg"
+			>
+				<Heading as="h1" fontSize="4xl" fontFamily="handwriting" mb="4">
+					¡Nos casamos!
+				</Heading>
+				<Text fontSize="2xl">Marta</Text>
+				<Text fontSize="2xl" fontFamily="handwriting">
+					&
+				</Text>
+				<Text fontSize="2xl">Mario</Text>
+			</Center>
 		</main>
 	)
 }
