72ee9bd31a3cd8b3a9c25967f43b1e55f6f79341
Mario Beltrán <belco90@gmail.com>
2024-10-13 15:43:33 +0200

    Improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,42 @@ import { Text } from '@/components/ui/text'
 import ampersandPic from '@/public/ampersand.png'
 
 import { css } from 'styled-system/css'
-import { Box, Center } from 'styled-system/jsx'
+import { Flex } from 'styled-system/jsx'
 
 export default function HomePage() {
 	return (
 		<main
 			className={css({ background: 'bg.emphasized', height: 'full', p: '8' })}
 		>
-			<Center flexDirection="column" justifyContent="center" color="accent.fg">
+			<Flex
+				flexDirection="column"
+				justifyContent="space-between"
+				alignItems="center"
+				color="accent.fg"
+				height="full"
+			>
 				<Heading
 					as="h1"
 					fontSize="4xl"
 					fontFamily="handwriting"
-					color="amber.a11"
+					color="yellow.a8"
 				>
 					¡Nos casamos!
 				</Heading>
-				<Box
-					display="flex"
+				<Flex
 					flexDirection="column"
 					justifyContent="center"
 					alignItems="center"
 					p="20"
+					my="10"
 					position="relative"
+					height="300px"
+					width="200px"
 				>
 					<Text
 						fontFamily="banner"
 						fontSize="6xl"
-						top="14"
+						top="-6"
 						position="absolute"
 						color="accent.a11"
 						zIndex="banner"
@@ -44,12 +52,13 @@ export default function HomePage() {
 						src={ampersandPic}
 						alt="and"
 						priority
+						fill
 						style={{ objectFit: 'cover', opacity: 0.6 }}
 					/>
 					<Text
 						fontFamily="banner"
 						fontSize="6xl"
-						bottom="16"
+						bottom="-4"
 						position="absolute"
 						color="accent.a11"
 						zIndex="banner"
@@ -57,11 +66,11 @@ export default function HomePage() {
 					>
 						Mario
 					</Text>
-				</Box>
+				</Flex>
 				<Text fontSize="lg" fontFamily="body" color="fg.default">
 					Más información próximamente
 				</Text>
-			</Center>
+			</Flex>
 		</main>
 	)
 }
