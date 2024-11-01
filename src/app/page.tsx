0e79ddcee1ac62145bf7b444a0497ae47f57a17d
Mario Beltrán <belco90@gmail.com>
2024-11-01 22:52:57 +0100

    Add celebration section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,38 @@
 import Image from 'next/image'
 import { PropsWithChildren } from 'react'
+import { FiHeart } from 'react-icons/fi'
+import { MdOutlineTapas, MdOutlineCelebration } from 'react-icons/md'
 
 import { Heading } from '@/components/ui/heading'
 import { Text } from '@/components/ui/text'
 import ampersandPic from '@/public/ampersand.png'
 
 import { css } from 'styled-system/css'
-import { Container, VStack } from 'styled-system/jsx'
+import { Box, Container, VStack } from 'styled-system/jsx'
 import type { ContainerProps } from 'styled-system/jsx'
 
 const Section = ({ children, ...props }: PropsWithChildren<ContainerProps>) => (
-	<Container maxW="xl" h="90vh" {...props}>
+	<Container maxW="xl" py="8" {...props}>
 		{children}
 	</Container>
 )
 
+const planningLiCss = css({
+	display: 'flex',
+	alignItems: 'center',
+	gap: 2,
+})
+
 export default function HomePage() {
 	return (
-		<main className={css({ height: 'full' })}>
-			<Section mb="env(safe-area-inset-bottom)">
+		<main
+			className={css({ height: '100vh', pb: 'env(safe-area-inset-bottom)' })}
+		>
+			<Section h="90vh">
 				<VStack height="full" justifyContent="space-around">
 					<Heading
 						as="h1"
-						fontSize="xl"
+						size="3xl"
 						fontFamily="handwriting"
 						color="fg.default"
 					>
@@ -82,13 +92,48 @@ export default function HomePage() {
 				</VStack>
 			</Section>
 
-			<Section bgColor="bg.emphasized" h="auto">
-				<VStack>
-					<Heading as="h2" py="8">
-						Más información próximamente
-					</Heading>
-				</VStack>
-			</Section>
+			<Box bgColor="bg.muted">
+				<Section>
+					<VStack alignItems="flex-start">
+						<Heading as="h2" size="2xl" fontFamily="handwriting">
+							Ceremonia y celebración
+						</Heading>
+						<Text>
+							¡Nos encantaría compartir contigo este día tan especial! La
+							ceremonia y la celebración tendrán lugar en Jardines de San Telmo.
+							Se ruega a los invitados llegar sobre las 12:30.
+						</Text>
+						<Box>
+							<Text>La planificación del evento es la siguiente:</Text>
+							<ul>
+								<li className={planningLiCss}>
+									<FiHeart />
+									<span>12:30 - Bienvenida y ceremonia</span>
+								</li>
+								<li className={planningLiCss}>
+									<MdOutlineTapas />
+									<span>14:00 - Cóctel y comida</span>
+								</li>
+								<li className={planningLiCss}>
+									<MdOutlineCelebration />
+									<span>18:00 - Fiesta</span>
+								</li>
+							</ul>
+						</Box>
+					</VStack>
+				</Section>
+			</Box>
+
+			<Box>
+				<Section>
+					<VStack alignItems="flex-start">
+						<Heading as="h2" size="2xl" fontFamily="handwriting">
+							Cómo llegar
+						</Heading>
+						<Text>Próximamente...</Text>
+					</VStack>
+				</Section>
+			</Box>
 		</main>
 	)
 }
