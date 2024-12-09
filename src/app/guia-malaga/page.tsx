b322d816cc7fd3c352791f100cccaf56bd3be1d6
Mario Beltrán <belco90@gmail.com>
2024-12-09 08:15:56 +0000

    Add metadata to guide page

--- src/app/guia-malaga/page.tsx
+++ src/app/guia-malaga/page.tsx
@@ -1,12 +1,22 @@
 import 'react-notion-x/src/styles.css'
+import type { Metadata } from 'next'
 import NextImage from 'next/image'
 import NextLink from 'next/link'
 
+import { openGraph } from '@/app/base-metadata'
 import { notionApi } from '@/notionApi'
 
 import { NotionClientRenderer } from './NotionClientRenderer'
 
 export const revalidate = 600 // 10 minutes
+
+const title = 'Guía de Málaga'
+const description = 'Recomendaciones para tu estancia en Málaga'
+export const metadata: Metadata = {
+	title,
+	description,
+	openGraph: { ...openGraph, title, description, url: '/guia-malaga' },
+}
 
 export default async function GuidePage() {
 	const notionPageId = process.env.NOTION_GUIDE_PAGE_ID ?? 'unknown'
